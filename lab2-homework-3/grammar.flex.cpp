8fb2c2c4e37c591277b06bc67bac90c4ce1c9ccb
Paul Katsoulas <pkatsoyl@gmail.com>
2023-11-01 07:48:41 +0200

    multiple line and single line is ready

--- lab2-homework-3/grammar.flex.cpp
+++ lab2-homework-3/grammar.flex.cpp
@@ -355,8 +355,8 @@ static void yynoreturn yy_fatal_error ( const char* msg  );
 	(yy_hold_char) = *yy_cp; \
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
-#define YY_NUM_RULES 7
-#define YY_END_OF_BUFFER 8
+#define YY_NUM_RULES 9
+#define YY_END_OF_BUFFER 10
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -364,11 +364,11 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[26] =
+static const flex_int16_t yy_accept[23] =
     {   0,
-        0,    0,    2,    2,    5,    5,    8,    7,    7,    2,
-        3,    2,    5,    5,    5,    4,    1,    2,    2,    1,
-        5,    5,    4,    1,    0
+        0,    0,    7,    7,    2,    2,   10,    9,    9,    7,
+        8,    2,    4,    3,    1,    6,    7,    2,    3,    3,
+        5,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -405,35 +405,37 @@ static const YY_CHAR yy_ec[256] =
 
 static const YY_CHAR yy_meta[5] =
     {   0,
-        1,    2,    1,    1
+        1,    2,    3,    4
     } ;
 
-static const flex_int16_t yy_base[29] =
+static const flex_int16_t yy_base[28] =
     {   0,
-       20,   19,    0,    0,    4,    0,   22,   25,    6,    0,
-       25,   10,    0,   17,   12,   25,   25,    0,    0,    0,
-        0,    0,    0,    0,   25,   16,   11,   18
+       24,   23,   23,   22,    0,    0,   20,   29,    2,    0,
+       29,    0,   29,    6,   29,   29,    0,    0,    0,    0,
+       29,   29,   10,   14,   18,   22,    7
     } ;
 
-static const flex_int16_t yy_def[29] =
+static const flex_int16_t yy_def[28] =
     {   0,
-       26,   26,   25,    3,   25,    5,   25,   25,   25,   27,
-       25,   25,   28,   28,   28,   25,   25,   27,   27,   27,
-       28,   28,   28,   28,    0,   25,   25,   25
+       23,   23,   24,   24,   22,    5,   22,   22,   22,   25,
+       22,   26,   22,   22,   22,   22,   25,   26,   27,   14,
+       22,    0,   22,   22,   22,   22,   22
     } ;
 
-static const flex_int16_t yy_nxt[30] =
+static const flex_int16_t yy_nxt[34] =
     {   0,
-       10,   11,   10,   12,   13,   13,   14,   15,   16,   17,
-       18,   18,   19,   20,   23,   24,    8,    8,   21,   21,
-       22,   25,    9,    9,    7,   25,   25,   25,   25
+       12,   13,   14,   12,   15,   16,   19,   19,   20,   21,
+        8,    8,    8,    8,   10,   10,   10,   10,   17,   22,
+       17,   17,   18,   11,   11,   18,    9,    9,    7,   22,
+       22,   22,   22
     } ;
 
-static const flex_int16_t yy_chk[30] =
+static const flex_int16_t yy_chk[34] =
     {   0,
-        3,    3,    3,    3,    5,    5,    5,    5,    9,    9,
-       12,   27,   12,   12,   15,   15,   26,   26,   28,   28,
-       14,    7,    2,    1,   25,   25,   25,   25,   25
+        5,    5,    5,    5,    9,    9,   14,   27,   14,   14,
+       23,   23,   23,   23,   24,   24,   24,   24,   25,    7,
+       25,   25,   26,    4,    3,   26,    2,    1,   22,   22,
+       22,   22,   22
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -451,8 +453,9 @@ int yy_flex_debug = 0;
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
 #line 1 "grammar.l"
+ 
 
-#line 455 "grammar.flex.cpp"
+#line 458 "grammar.flex.cpp"
 
 #define INITIAL 0
 #define SINGLELINECOMMENTS 1
@@ -668,9 +671,11 @@ YY_DECL
 		}
 
 	{
-#line 3 "grammar.l"
+#line 4 "grammar.l"
 
-#line 673 "grammar.flex.cpp"
+
+
+#line 678 "grammar.flex.cpp"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -697,13 +702,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 26 )
+				if ( yy_current_state >= 23 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 25 );
+		while ( yy_base[yy_current_state] != 29 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -729,42 +734,52 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 4 "grammar.l"
-{ BEGIN (SINGLELINECOMMENTS) ;}
+#line 7 "grammar.l"
+{ BEGIN (MULTIPLELINECOMMENTS) ;}
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 5 "grammar.l"
-{ printf("i find it\n"); } 
+#line 8 "grammar.l"
+{ printf("(multiple lines comments)\n"); } 
 	YY_BREAK
 case 3:
-/* rule 3 can match eol */
-YY_RULE_SETUP
-#line 6 "grammar.l"
-{ BEGIN (INITIAL); } 
-	YY_BREAK
-case 4:
-YY_RULE_SETUP
-#line 8 "grammar.l"
-{ BEGIN (MULTIPLELINECOMMENTS) ;}
-	YY_BREAK
-case 5:
-/* rule 5 can match eol */
 YY_RULE_SETUP
 #line 9 "grammar.l"
-{ printf("i find it too\n"); } 
+
+	YY_BREAK
+case 4:
+/* rule 4 can match eol */
+YY_RULE_SETUP
+#line 10 "grammar.l"
+
+	YY_BREAK
+case 5:
+YY_RULE_SETUP
+#line 11 "grammar.l"
+{ BEGIN (INITIAL); } 
 	YY_BREAK
 case 6:
 YY_RULE_SETUP
-#line 10 "grammar.l"
-{ BEGIN (INITIAL); } 
+#line 13 "grammar.l"
+{ BEGIN (SINGLELINECOMMENTS) ;}
 	YY_BREAK
 case 7:
 YY_RULE_SETUP
-#line 12 "grammar.l"
+#line 14 "grammar.l"
+{ printf("(single line comments)\n"); } 
+	YY_BREAK
+case 8:
+/* rule 8 can match eol */
+YY_RULE_SETUP
+#line 15 "grammar.l"
+{ BEGIN (INITIAL); } 
+	YY_BREAK
+case 9:
+YY_RULE_SETUP
+#line 18 "grammar.l"
 ECHO;
 	YY_BREAK
-#line 767 "grammar.flex.cpp"
+#line 782 "grammar.flex.cpp"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(SINGLELINECOMMENTS):
 case YY_STATE_EOF(MULTIPLELINECOMMENTS):
@@ -1063,7 +1078,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 26 )
+			if ( yy_current_state >= 23 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1091,11 +1106,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 26 )
+		if ( yy_current_state >= 23 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 25);
+	yy_is_jam = (yy_current_state == 22);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1771,7 +1786,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 12 "grammar.l"
+#line 18 "grammar.l"
 
 
 
